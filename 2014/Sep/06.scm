db394e34921b5cc5d80de7abc724e081f012e3fb
tgfjt <fujita@tagpanda.co.jp>
2014-09-06 22:53:09 +0900

    ファイル出力してみる。

--- 2014/Sep/06.scm
+++ 2014/Sep/06.scm
@@ -1,16 +1,27 @@
+;; 06 Sep 2014
+
 (use text.html-lite)
 (use text.tree)
-(define out (open-output-file "2014/Sep/06.html"))
-	(write-tree
-	 `(,(html-doctype :type :html-5)
-		 ,(html:html
-			 (html:head (html:title "Gauche Studying Sep/06"))
-			 (html:body
-				(html:div
-				 (html:p "HTML LINK")
-				 (html:a :href "http://www.tagpanda.co.jp/"
-								 "TAG PANDA inc."))
-				(html:footer
-				 (html:p "copyright &copy; tgfjt")))))
- out)
- (close-output-port out)
+
+(define (sep06 path)
+  (define out (open-output-file path))
+  (write-tree
+   `(,(html-doctype :type :html-5)
+     ,(html:html
+       (html:head (html:title "Gauche Studying Sep/06"))
+       (html:body
+        (html:h1 "output HTML!!")
+        (html:div
+         (html:p :class "hoge fuga"
+                 "interesting tag element indent")
+         (html:a :href "http://www.tagpanda.co.jp/" :target "_blank"
+                 "TAG PANDA inc."))
+        (html:footer
+         (html:p "copyright &copy; tgfjt")))))
+   out)
+  (close-output-port out))
+
+(define (main args)
+  (sep06 "2014/Sep/06.html")
+  (print "Createted: 2014/Sep/06.html")
+  0)
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # to study Gauche
 
+## REPL
+
+`gosh` or `rlwrap gosh`
+
+## Run
+
+`gosh xxx.scm`
+
+## Gauche ドキュメント
+
+### Chrome
+
+- `chrome://settings/searchEngines` を開いて、
+- gosh: `http://practical-scheme.net/gauche/man/?l=jp&p=%s` を設定。
+
